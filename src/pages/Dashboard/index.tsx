04c185dc4278102a21009cf07cacda03405edef4
Thiago Assis <thiagosil@gmail.com>
2020-07-01 19:06:12 -0300

    Initial listings

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,6 +35,7 @@ interface Food {
   price: number;
   thumbnail_url: string;
   formattedPrice: string;
+  category: number;
 }
 
 interface Category {
@@ -54,12 +55,22 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      api
+        .get<Food[]>('foods', {
+          params: { category: selectedCategory, name_like: searchValue },
+        })
+        .then(response => {
+          const formattedFood = response.data.map(food => ({
+            ...food,
+            formattedPrice: formatValue(food.price),
+          }));
+          setFoods(formattedFood);
+        });
     }
 
     loadFoods();
@@ -67,14 +78,17 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      api.get<Category[]>('categories').then(response => {
+        setCategories(response.data);
+      });
     }
 
     loadCategories();
   }, []);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    const newSelectedCategory = selectedCategory !== id ? id : undefined;
+    setSelectedCategory(newSelectedCategory);
   }
 
   return (
--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -33,6 +33,13 @@ const Favorites: React.FC = () => {
   useEffect(() => {
     async function loadFavorites(): Promise<void> {
       // Load favorite foods from api
+      api.get<Food[]>('favorites').then(({ data }) => {
+        const formattedFoods = data.map(food => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        }));
+        setFavorites(formattedFoods);
+      });
     }
 
     loadFavorites();
